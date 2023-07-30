81e56e99a0f450260879592a739eb3181c4950c3
Sebastian-Andersson <sebastian@cshift.tech>
2023-07-30 11:21:36 +0200

    top done next is footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p className="header-text">BYTEBURST</p>
       </div>
       <div className="banner"></div>
+      <div className="banner2">
+      <p className="banner2-text">FUELING TECH EXCELLENCE ONE BYTE AT A TIME</p>
+      </div>
     </div>
   );
 }
